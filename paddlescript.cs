ce06ca871b9a47073c559bdb44e44ae7950ce3de
Vipul Tamboli <68987811+VipulTamboli@users.noreply.github.com>
2021-02-05 14:15:50 +0530

    paddlescript.cs

---
+++ paddlescript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour
+{
+      Rigidbody2D rb;
+      public float movespeed;
+
+     private void Awake()
+     {
+       rb = GetComponent<Rigidbody2D>();
+       }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        TouchMove();
+    }
+
+   void TouchMove()
+    {
+      if (Input.GetMouseButton(0))
+         {
+          Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+          
+          if (touchPos.x < 0)
+          {
+           rb.velocity = Vector2.left * movespeed;
+           }
+           else
+             {
+                rb.velocity = Vector2.right * movespeed;
+              }
+         }
+         else
+           {
+             rb.velocity = Vector2.zero;
+               }
+      }
+}
